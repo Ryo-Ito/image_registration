8a9a9e987516c4f2b43bd78077d3e3d79bcc62f9
ryoito <r0735nj5058@gmail.com>
2016-07-08 15:15:37 +0900

    added __all__

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from misc import identity_mapping, jacobian_matrix, determinant
 from imageprocessing import gradient, uniform_filter, interpolate_mapping, sliding_matrix_product
 import deformation, image, registration, regularizer
+__all__ = ["identity_mapping", "jacobian_matrix", "determinant", "gradient", "uniform_filter", "interpolate_mapping", "sliding_matrix_product", "deformation", "image", "registration", "regularizer"]
