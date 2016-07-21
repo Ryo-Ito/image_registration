34c14b877e18ac25766a922624665fe67d1c824f
ryoito <r0735nj5058@gmail.com>
2016-07-21 15:10:13 +0900

    renanmed functions

--- rtk/__init__.py
+++ rtk/__init__.py
@@ -1,9 +1,20 @@
 from misc import identity_mapping, jacobian_matrix, determinant
-from imageprocessing import (gradient, uniform_filter,
-                             interpolate_mapping, sliding_matrix_product)
+from imageprocessing import (gradient, uniform_convolve,
+                             interpolate_mapping, sliding_matmul)
 import deformation
 import image
 import registration
 import regularizer
 import similarity
-__all__ = ["identity_mapping", "jacobian_matrix", "determinant", "gradient", "uniform_filter", "interpolate_mapping", "sliding_matrix_product", "deformation", "image", "registration", "regularizer", "similarity"]
+__all__ = ["identity_mapping",
+           "jacobian_matrix",
+           "determinant",
+           "gradient",
+           "uniform_convolve",
+           "interpolate_mapping",
+           "sliding_matmul",
+           "deformation",
+           "image",
+           "registration",
+           "regularizer",
+           "similarity"]
