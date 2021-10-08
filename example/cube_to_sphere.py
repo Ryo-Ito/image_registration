931a32e5fe832b650e5f29c143804b757dcbffd9
Calder Sheagren <caldersheagren@gmail.com>
2021-10-08 13:51:11 -0400

    fix bugs

--- example/cube_to_sphere.py
+++ example/cube_to_sphere.py
@@ -2,14 +2,13 @@
 import rtk
 import numpy as np
 
-
-# fixed_img = rtk.ScalarImage(data=fixed_image) # isinstance(fixed_image, np.ndarray) = True
-# moving_img = rtk.ScalarImage(data=moving_image) # isinstance(moving_image, np.ndarray) = True
-
 fixed_img = rtk.load_img("data/sphere.nii")
 moving_img = rtk.load_img("data/cube.nii")
+
 similarity = rtk.similarity.SSD(1000.)
+
 regularizer = rtk.regularizer.BiharmonicRegularizer(convexity_penalty=1., norm_penalty=1.)
+
 model = rtk.registration.LDDMM(
     n_step=32,
     regularizer=regularizer,
@@ -21,9 +20,13 @@ model = rtk.registration.LDDMM(
     unit_threshold=0.1,
     learning_rate=0.1,
     n_jobs=4)
+
 model.set_images(fixed_img, moving_img)
+
 warp = model.execute()
+
 warped_img = moving_img.apply_transform(warp)
 
 np.save('warp_grid.npy', warp.grid)
+
 rtk.save(warped_img, "cube_to_sphere.nii")
